5e7e4f09fa7cbce9b5ec08a206fd70c693a108a8
Ivan Font <ivan@ivanfont.com>
2017-01-02 00:33:06 -0800

    Add additional tmux, vim, and bash stuff to script

--- bash-it/custom.bash
+++ bash-it/custom.bash
@@ -1,8 +1,24 @@
 #!/usr/bin/env bash
 
 # User specific settings
+
+# If set to ‘on’, words which have more than one possible completion
+# cause the matches to be listed immediately instead of ringing the
+# bell. The default value is ‘off’.
+bind 'set show-all-if-ambiguous on'
+
+# Environment variables
+export EDITOR='vim'
 export HISTFILESIZE=100000
+export HISTCONTROL=ignorespace:ignoredups
 
 # Vagrant variables
 export VAGRANT_DEFAULT_PROVIDER=libvirt
 
+# Support for X11-clipboard and resizing panes through tmux when using Fedora
+if [ -e /usr/bin/vimx ]; then
+    alias vim='/usr/bin/vimx'
+fi
+
+# Disable scroll lock so as not be disturbed by ctrl-S ctrl-Q in terminals:
+stty -ixon
