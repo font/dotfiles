e1a0c919f82694a3939015c6ed817ef31ed6103f
Ivan Font <ivan@ivanfont.com>
2017-01-25 10:48:22 -0800

    Update font_priv theme until PR is merged

--- bash-it/themes/font_priv/font_priv.theme.bash
+++ bash-it/themes/font_priv/font_priv.theme.bash
@@ -47,7 +47,8 @@ function prompt_command() {
     # Append new history lines to history file
     history -a
 
-    PS1="${virtualenv}${bold_cyan}\W $(scm_prompt_char_info)${ret_status}→ ${normal}"
+    #PS1="${virtualenv}${bold_cyan}\W $(scm_prompt_char_info)${ret_status}→ ${normal}"
+    PS1="${virtualenv}${bold_cyan}\W $(scm_char)$(scm_prompt_info)${ret_status}→ ${normal}"
 }
 
 unset PROMPT_COMMAND
