fb870386173b6daf16fb1787dc2d496aaaefad2e
Ivan Font <ivan@ivanfont.com>
2017-07-19 17:41:08 -0700

    Add mutt alias for saving stuff to downloads dir

--- bash-it/custom.aliases.bash
+++ bash-it/custom.aliases.bash
@@ -5,3 +5,6 @@ alias vi='vim'
 
 # Virsh
 alias virshrmvollist='for i in $(sudo virsh vol-list default 2>&1 | awk "/^ disk/ {print $2}"); do sudo virsh vol-delete $i; done'
+
+# mutt
+alias mutt='cd ~/.mutt/downloads && mutt'
