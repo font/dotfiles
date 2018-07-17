e65dccba0798314ef7f2e2a3e305958b3a737768
Ivan Font <ivan@ivanfont.com>
2018-07-17 13:33:04 -0700

    Update bash PATH, completions and aliases

--- bash-it/custom.aliases.bash
+++ bash-it/custom.aliases.bash
@@ -3,6 +3,10 @@ alias mv='mv -i'
 alias rm='rm -i'
 alias vi='vim'
 
+# Kubernetes
+alias cdfed='cd ~/go/src/github.com/kubernetes-sigs/federation-v2'
+alias cdkube='cd ~/go/src/k8s.io/kubernetes'
+
 # Virsh
 alias virshrmvollist='for i in $(sudo virsh vol-list default 2>&1 | awk "/^ disk/ {print $2}"); do sudo virsh vol-delete $i; done'
 
