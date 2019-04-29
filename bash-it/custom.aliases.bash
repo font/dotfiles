d65f47aba8687115b950723b494156c9ff7685d6
Ivan Font <ifont@redhat.com>
2019-04-29 16:58:44 -0700

    Update custom k8s aliases

--- bash-it/custom.aliases.bash
+++ bash-it/custom.aliases.bash
@@ -4,8 +4,11 @@ alias rm='rm -i'
 alias vi='vim'
 
 # Kubernetes
-alias cdfed='cd ~/go/src/github.com/kubernetes-sigs/federation-v2'
 alias cdkube='cd ~/go/src/k8s.io/kubernetes'
+
+# Kubernetes Federation
+alias cdfed='cd ~/go/src/github.com/kubernetes-sigs/federation-v2'
+alias kf='kubefed2'
 
 # Virsh
 alias virshrmvollist='for i in $(sudo virsh vol-list default 2>&1 | awk "/^ disk/ {print $2}"); do sudo virsh vol-delete $i; done'
